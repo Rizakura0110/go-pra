8e09223444dfce86d0a43249ec417324b0132e75
ryo sasakura <rasakura0110@gmail.com>
2022-07-05 23:47:03 +0900

    8

--- 8/main.go
+++ 8/main.go
@@ -23,4 +23,70 @@ func main() {
 		fmt.Println(err)
 	}
 	fmt.Printf("i = %T\n", i)
+
+	// for
+	point := 0
+	for point < 10 {
+		fmt.Println(point)
+		point++
+	}
+
+	for i := 0; i < 10; i++ {
+		if i == 3 {
+			continue
+		}
+		if i == 6 {
+			break
+		}
+		fmt.Println(i)
+	}
+
+	arr := [3]int{1, 2, 3}
+	for i := 0; i < len(arr); i++ {
+		println(arr[i])
+	}
+
+	arr2 := [3]int{1, 2, 3}
+	for i, v := range arr2 {
+		fmt.Println(i, v)
+	}
+
+	arr3 := [3]int{1, 2, 3}
+	for _, v := range arr3 {
+		fmt.Println(v)
+	}
+
+	sl := []string{"test", "pc", "apple"}
+	for i, v := range sl {
+		fmt.Println(i, v)
+	}
+
+	m := map[string]int{"apple": 100, "banana": 200}
+
+	for k := range m {
+		fmt.Println(k)
+	}
+
+	// switch
+	/*
+		n := 8
+		switch n {
+		case 1, 2:
+			fmt.Println("1 or 2")
+		case 3, 4:
+			fmt.Println("3 or 4")
+		default:
+			fmt.Println("no")
+
+		}
+	*/
+
+	switch n := 2; n {
+	case 1, 2:
+		fmt.Println("1 or 2")
+	case 3, 4:
+		fmt.Println("3 or 4")
+	default:
+		fmt.Println("no")
+	}
 }
